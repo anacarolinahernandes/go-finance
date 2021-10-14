4cd5d0e4af7ba8a8c58d8a0813c5f0631275c6a9
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2021-10-14 17:47:28 -0300

    adicionando fontes estilizadas e criando componentes com styled components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/pages/Dashboard';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <Dashboard />
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
 
